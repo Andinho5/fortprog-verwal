d406929b9acc80241895aba9522c40632e6d5b52
moritz2309 <moritz.schmidt@hsw-stud.de>
2024-11-22 16:11:03 +0100

    Added big ascii title

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class Main {
+
+
+    public static void main(String[] args) {
+        ClassLoader classLoader = Main.class.getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream("Title");
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))){
+            while(reader.ready()){
+                System.out.println(reader.readLine());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println();
     }
 }
